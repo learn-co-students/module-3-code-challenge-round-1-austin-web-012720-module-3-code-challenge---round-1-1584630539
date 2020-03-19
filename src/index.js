9670b7f05f425d75870b14fbe377b652960852f0
qudroturkey <reesemb1@gmail.com>
2020-03-19 11:43:32 -0500

    what even is a post request

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
+/* eslint-disable no-console */
+/* eslint-disable no-use-before-define */
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta');
 
-  let imageId = 1 //Enter the id from the fetched image here
+  const imageId = 4896; // Enter the id from the fetched image here
+  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`;
+  const likeURL = 'https://randopic.herokuapp.com/likes/';
+  const commentsURL = 'https://randopic.herokuapp.com/comments/';
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+  const imageSrc = document.getElementById('image');
+  const imageTitle = document.getElementById('name');
+  const likeCount = document.getElementById('likes');
+  const commentsContainer = document.getElementById('comments');
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+  const likeBtn = document.getElementById('like_button');
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+  imageFetch();
 
-})
+  function imageFetch() {
+    fetch(imageURL)
+      .then((resp) => resp.json())
+      .then((json) => addImage(json));
+  }
+
+  function addImage(json) {
+    console.log(json);
+    imageSrc.src = json.url;
+    imageTitle.innerText = json.name;
+    likeCount.innerText = json.like_count;
+  }
+
+  likeBtn.addEventListener('click', () => {
+    likeCount.innerText++;
+    // persistLikes();
+  });
+
+
+
+
+
+  // function persistLikes() {
+  //   fetch(likeURL, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       Accept: 'application/json',
+  //     },
+  //     body: JSON.stringify
+
+  //       ,
+  //   }).then((resp) => resp.json())
+  //   .then((json) => addImage(json));
+  // }
+
+  // was tring to get likes to persist in the backend, ran out of time.
+});
