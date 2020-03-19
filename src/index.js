961043c31df99690b8ce62a6711406a0dc451587
lughjw <lughjw@gmail.com>
2020-03-19 11:48:15 -0500

    Finished

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 4903 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,92 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  // const imageEl = document.createElement('img')
+  // imageEl.src = imageURL
+
+  document.getElementById('like_button').addEventListener('click', event => {
+    
+    // console.log(event.target)
+    const likesEl = document.getElementById('likes')
+    likesEl.innerText = parseInt(likesEl.innerText) + 1
+    
+     fetch(`${likeURL}`, {
+    // fetch(`${likeURL}/${imageId}`, {
+    //  fetch(`${imageURL}`, {
+      method: 'POST',
+      headers: {
+        'Content-type' : 'application/json',
+        'Accept' : 'application/json'
+      },
+      body: JSON.stringify({'image_id': imageId})
+    })
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+  })
+
+  const commentForm = document.getElementById('comment_form')
+  commentForm.addEventListener('submit', event => {
+    event.preventDefault()
+    
+    const commentText = document.getElementById('comment_input').value
+    
+    const commentLi = document.createElement('li')
+    commentLi.innerHTML = commentText
+    document.getElementById('comments').appendChild(commentLi)
+
+    fetch(`${commentsURL}`, {
+      method: 'POST',
+      headers: {
+        'Content-type' : 'application/json',
+        'Accept' : 'application/json'
+      },
+      body: JSON.stringify({
+        'content': commentText,
+        'image_id': imageId
+      })
+    })
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+    
+  })
+
+  
+  fetch(imageURL)
+  .then(resp => resp.json())
+  .then(imageObj => {
+    displayImage(imageObj)
+    displayLikes(imageObj)
+    displayComments(imageObj)
+  })
+
+  // const imageEl = document.getElementById('image')
+  // imageEl.src = imageURL
+  // imageEl.dataId = imageId
+
 })
+
+function displayImage(imageObj) {
+  // console.log(imageObj)
+  const imageEl = document.getElementById('image')
+  imageEl.src = imageObj.url
+  imageEl.dataId = imageObj.id
+
+  document.getElementById('name').innerText = imageObj.name
+}
+
+function displayLikes(imageObj) {
+  // console.log(imageObj)
+
+  document.getElementById('likes').innerText = imageObj.like_count
+
+}
+
+function displayComments(imageObj) {
+  const commentsUL = document.getElementById('comments')
+  imageObj.comments.forEach(comment => {
+    const commentEl = document.createElement('li')
+    commentEl.innerText = comment.content
+
+    commentsUL.appendChild(commentEl)
+  })
+}
